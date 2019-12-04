09ec3f355579a17a4dfed52df4b3fb9beacb5265
ryan-tech <rkim@nevada.unr.edu>
2019-12-04 02:04:13 -0800

    Added object viewer and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 Libraries used:
 Tkinter
+OpenGL
+Pygame
+
+Controls in Object Viewer:
+    
+    LMB: rotate object
+    RMB: translate object
+    i : zoom in
+    o : zoom out 
--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,28 @@
 import tkinter as tk
-window = tk.Tk()
-window.title('Cylindrical Object Slicer')
-window.mainloop()
+from tkinter import filedialog
+from objviewer import objView
+
+class SlicerGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Slicer")
+
+        self.label = tk.Label(master, text="Load a wavefront 3D object file.")
+        self.label.pack()
+
+        self.greet_button = tk.Button(master, text="Open .obj File", command=self.openFile)
+        self.greet_button.pack()
+
+        self.close_button = tk.Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def openFile(self):
+        self.filename = tk.filedialog.askopenfilename(initialdir="/", title="Select file",
+                                                      filetypes=(("Object files", "*.obj"), ("all files", "*.*")))
+        objView(self.filename)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    gui = SlicerGUI(root)
+    root.mainloop()
