dd3bc4893954af789ecb43b3adf26cbd3f8c82b6
Ryan <rkim@nevada.unr.edu>
2020-03-09 15:06:42 -0700

    rk- intermediate commit

---
+++ C-version/build/CMakeFiles/fstl.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "fstl_autogen"
+  "CMakeFiles/fstl_autogen.dir/AutogenOldSettings.cmake"
+  "qrc_qt.cpp"
+  "qrc_gl.cpp"
+  "CMakeFiles/fstl.dir/src/app.cpp.o"
+  "CMakeFiles/fstl.dir/src/backdrop.cpp.o"
+  "CMakeFiles/fstl.dir/src/canvas.cpp.o"
+  "CMakeFiles/fstl.dir/src/glmesh.cpp.o"
+  "CMakeFiles/fstl.dir/src/loader.cpp.o"
+  "CMakeFiles/fstl.dir/src/main.cpp.o"
+  "CMakeFiles/fstl.dir/src/mesh.cpp.o"
+  "CMakeFiles/fstl.dir/src/window.cpp.o"
+  "CMakeFiles/fstl.dir/qrc_qt.cpp.o"
+  "CMakeFiles/fstl.dir/qrc_gl.cpp.o"
+  "CMakeFiles/fstl.dir/fstl_autogen/mocs_compilation.cpp.o"
+  "fstl.pdb"
+  "fstl"
+  "fstl.pdb"
+  "CMakeFiles/CMakeRelink.dir/fstl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fstl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
