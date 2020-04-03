c0ec2eea18630ac719a80605fc811b353cd4542b
dvan-unr <58541555+dvan-unr@users.noreply.github.com>
2020-04-02 22:07:56 -0700

    Update Vector.h

--- C/src/Vector.h
+++ C/src/Vector.h
@@ -9,31 +9,39 @@
  */
 struct Vector
 {
-    Vector() {}
+    Vector() : x(0.0f), y(0.0f), z(0.0f) {}
     Vector(float x, float y, float z) : x(x), y(y), z(z) {}
-    Vector(Vector& v) : x(v.x), y(v.y), z(v.z) {}
+    Vector(const Vector& v) : x(v.x), y(v.y), z(v.z) {}
 
-    GLfloat x, y, z;
-    GLuint i=0;
+    float x, y, z;
+    int i=0;
 
-    Vector operator+(const Vector& a, const Vector& b);
-    Vector operator-(const Vector& a, const Vector& b);
-    Vector operator*(const Vector& v, float c);
-    Vector operator/(const Vector& v, float c);
-
-    std::ostream& operator<<(std::ostream& os, const Vector& v);
-
-    bool operator==(const Vector& a, const Vector& b);
-    bool operator!=(const Vector& a, const Vector& b);
-
-    float dotProduct(const Vector& a, const Vector& b);
-    float crossProduct(const Vector& a, const Vector& b);
-    float calcDistance(const Vector& a, const Vector& b);
-
-    float calcMagnitude(const Vector& v);
-
-    void normalize(const Vector& v);
-    Vector normalized(const Vector& v);
+    Vector& operator=(const Vector& v)
+    {
+    x = v.x;
+    y = v.y;
+    z = v.z;
+    return *this;
+    }
 };
+
+Vector operator+(const Vector& a, const Vector& b);
+Vector operator-(const Vector& a, const Vector& b);
+Vector operator*(const Vector& v, float c);
+Vector operator/(const Vector& v, float c);
+
+std::ostream& operator<<(std::ostream& os, const Vector& v);
+
+bool operator==(const Vector& a, const Vector& b);
+bool operator!=(const Vector& a, const Vector& b);
+
+float dotProduct(const Vector& a, const Vector& b);
+Vector crossProduct(const Vector& a, const Vector& b);
+float calcDistance(const Vector& a, const Vector& b);
+
+float calcMagnitude(const Vector& v);
+
+void normalize(Vector& v);
+Vector normalized(const Vector& v);
 
 #endif
